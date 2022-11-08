08e4ee7d5ba657b028fccbf7fa48eb724654008a
Coding-Ocean <eijo524@gmail.com>
2022-11-08 16:59:56 +0900

    change variable name

--- appOneLT/gmain.cpp
+++ appOneLT/gmain.cpp
@@ -11,7 +11,7 @@ void gmain()
     colorMode(HSV, 100);
     angleMode(DEGREES);
     COLOR black(0, 0, 0);
-    COLOR white(0, 0, 100, 50);
+    COLOR white(0, 0, 100, 30);
     COLOR blue(220, 100, 100);
     COLOR red(330, 100, 100);
     COLOR yellow(65, 100, 80);
@@ -26,16 +26,15 @@ void gmain()
     addPointToMoveWithMouse(&p2);
     addPointToMoveWithMouse(&p3);
     //Lerped Points
-    VECTOR a, b, c, d, e, p, sp;
     float t = 0;
     float dt = 0.005f;
     const float DT = dt;
     //Animation Switch
     const int POINTS = 0;
-    const int MOVE_CUBIC = 1;
-    const int STOP_CUBIC = 2;
-    const int MOVE_QUADRATIC = 3;
-    const int STOP_QUADRATIC = 4;
+    const int DYNAMIC_CUBIC = 1;
+    const int STATIC_CUBIC = 2;
+    const int DYNAMIC_QUADRATIC = 3;
+    const int STATIC_QUADRATIC = 4;
     int sw = POINTS;
     bool advance = true;
     //Main Loop
@@ -45,39 +44,56 @@ void gmain()
         movePointWithMouse();
 
         //Switch animation
-        if (isTrigger(KEY_D)) { ++sw %= 5; }
-        if (isTrigger(KEY_A)) { --sw; if (sw < 0)sw = 4; }
-        if (sw == POINTS) { t = 0; }
-        if (sw == MOVE_CUBIC || sw == MOVE_QUADRATIC) {
-            if (isTrigger(KEY_SPACE))advance = !advance;
+        if (isTrigger(KEY_D)) { 
+            ++sw %= 5; 
+        }
+        if (isTrigger(KEY_A)) { 
+            --sw; 
+            if (sw < 0)sw = 4; 
+        }
+        if (sw == POINTS) {
+            t = 0;
+        }
+        if( sw == STATIC_CUBIC || 
+            sw == STATIC_QUADRATIC) { 
+            t = 1; 
+        }
+        if (sw == DYNAMIC_CUBIC || 
+            sw == DYNAMIC_QUADRATIC) {
+            if (isTrigger(KEY_S)) {
+                advance = !advance;
+            }
             if (advance) {
                 t += dt;
-                if (t < 0 || t > 1) dt = -dt;
+                if (t < 0 || t > 1) {
+                    dt = -dt;
+                }
             }
         }
-        if (sw == STOP_CUBIC || sw == STOP_QUADRATIC) { t = 1; }
         //Bezier Curve
-        sp = p0;
+        VECTOR a = p0, b = p1, c = p2, d = p0, e = p1, p = p0;
+        VECTOR sp = p0;//start position
         for (float tm = DT; tm <= t; tm += DT) {
             a = lerp(p0, p1, tm);
             b = lerp(p1, p2, tm);
             c = lerp(p2, p3, tm);
-            d = lerp(a, b, tm);
-            e = lerp(b, c, tm);
-            p = lerp(d, e, tm);
-            if (sw == MOVE_CUBIC)
-                line(sp, p, yellow, 5);
-            if (sw == STOP_CUBIC)
-                line(sp, p, yellow, 20);
-            if (sw == MOVE_QUADRATIC || sw == STOP_QUADRATIC) {
+            if (sw == DYNAMIC_QUADRATIC || 
+                sw == STATIC_QUADRATIC) {
                 rainbow.set(360 * tm, 100, 100, 20);
                 line(a, b, rainbow, 1);
                 line(b, c, rainbow, 1);
             }
+            d = lerp(a, b, tm);
+            e = lerp(b, c, tm);
+            p = lerp(d, e, tm);
+            if (sw == DYNAMIC_CUBIC||
+                sw == STATIC_CUBIC){
+                line(sp, p, yellow, 15);
+            }
             sp = p;
         }
         //Bezier Points
-        if (sw == MOVE_CUBIC) {
+        if (sw == DYNAMIC_CUBIC) {
             line(a, b, blue, 2);
             line(b, c, blue, 2);
             line(d, e, red, 2);
