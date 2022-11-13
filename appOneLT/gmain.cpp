81b778f43f84237f9dd01f00c9b15f603638f17e
Coding-Ocean <eijo524@gmail.com>
2022-11-13 13:28:04 +0900

    derivative sample

--- appOneLT/gmain.cpp
+++ appOneLT/gmain.cpp
@@ -1,11 +1,11 @@
-//#define _BEZIER
-#define _DERIVATIVE
+#define _BEZIER
+//#define _DERIVATIVE
 #ifdef _BEZIER
 #include"libOne.h"
 #include"moveWithMouse.h"
 VECTOR lerp(VECTOR& p0, VECTOR& p1, float t)
 {
-    return (1 - t) * p0 + t * p1;
+    return p0 + (p1 - p0) * t;
 }
 void gmain()
 {
@@ -14,7 +14,7 @@ void gmain()
     colorMode(HSV, 100);
     angleMode(DEGREES);
     COLOR black(0, 0, 0);
-    COLOR white(0, 0, 100, 30);
+    COLOR white(0, 0, 100, 50);
     COLOR blue(220, 100, 100);
     COLOR red(330, 100, 100);
     COLOR yellow(65, 100, 80);
@@ -28,17 +28,16 @@ void gmain()
     addPointToMoveWithMouse(&p1);
     addPointToMoveWithMouse(&p2);
     addPointToMoveWithMouse(&p3);
-    //Lerped Points
+    //
     float t = 0;
     float dt = 0.005f;
     const float DT = dt;
     //Animation Switch
-    const int POINTS = 0;
+    const int STATIC_CUBIC = 0;
     const int DYNAMIC_CUBIC = 1;
-    const int STATIC_CUBIC = 2;
+    const int STATIC_QUADRATIC = 2;
     const int DYNAMIC_QUADRATIC = 3;
-    const int STATIC_QUADRATIC = 4;
-    int sw = POINTS;
+    int sw = STATIC_CUBIC;
     bool advance = true;
     //Main Loop
     while (notQuit) {
@@ -48,14 +47,12 @@ void gmain()
 
         //Switch animation
         if (isTrigger(KEY_D)) { 
-            ++sw %= 5; 
+            ++sw %= 4; 
+            t = 0;
         }
         if (isTrigger(KEY_A)) { 
             --sw; 
-            if (sw < 0)sw = 4; 
-        }
-        if (sw == POINTS) {
-            t = 0;
+            if (sw < 0)sw = 3; 
         }
         if( sw == STATIC_CUBIC || 
             sw == STATIC_QUADRATIC) { 
@@ -80,18 +77,18 @@ void gmain()
             a = lerp(p0, p1, tm);
             b = lerp(p1, p2, tm);
             c = lerp(p2, p3, tm);
-            if (sw == DYNAMIC_QUADRATIC || 
+            d = lerp(a, b, tm);
+            e = lerp(b, c, tm);
+            p = lerp(d, e, tm);
+            if (sw == DYNAMIC_QUADRATIC ||
                 sw == STATIC_QUADRATIC) {
                 rainbow.set(360 * tm, 100, 100, 20);
                 line(a, b, rainbow, 1);
                 line(b, c, rainbow, 1);
             }
-            d = lerp(a, b, tm);
-            e = lerp(b, c, tm);
-            p = lerp(d, e, tm);
-            if (sw == DYNAMIC_CUBIC||
+            else if (sw == DYNAMIC_CUBIC||
                 sw == STATIC_CUBIC){
-                line(sp, p, yellow, 15);
+                line(sp, p, yellow, 10);
             }
             sp = p;
         }
@@ -115,6 +112,9 @@ void gmain()
         circle(p1, 14, black, white, 6);
         circle(p2, 14, black, white, 6);
         circle(p3, 14, black, white, 6);
+
+        fill(white);
+        print("Push [d] to change scene.");
     }
 }
 #endif
@@ -136,24 +136,26 @@ void gmain()
     COLOR yellow(65, 100, 80);
     COLOR rainbow;
     //Points
-    VECTOR p0(600, 800);
-    VECTOR p1(950, 200);
-    VECTOR p2(1300, 800);
-    VECTOR p3(1650, 200);
+    VECTOR p0(600, 500);
+    VECTOR p1(933, 500);
+    VECTOR p2(1266, 500);
+    VECTOR p3(1600, 500);
     addPointToMoveWithMouse(&p0);
     addPointToMoveWithMouse(&p1);
     addPointToMoveWithMouse(&p2);
     addPointToMoveWithMouse(&p3);
-    std::vector<float> LUT;
+    //tでキャラを動かす
     float t = 0;
     int img = loadImage("sukebo.png");
+    //distでキャラを動かす
+    std::vector<float> LUT;
     float dist = 0;
     int imgBlue = loadImage("sukeboBlue.png");
     //Main Loop
     while (notQuit) {
-        clear();
         movePointWithMouse();
-        
+        clear();
+        //３次ベジェ曲線を描く
         {
             VECTOR sp = p0;
             VECTOR ep;
@@ -165,36 +167,38 @@ void gmain()
                 ep = bezier(p0, p1, p2, p3, t);
                 line(sp, ep, yellow, 5);
                 point(sp, yellow, 15);
+                //距離の累計をテーブルに保存
                 dist += (sp - ep).mag();
                 LUT.push_back(dist);
-                fill(white); printPosX(400); print(dist);
+                fill(white); printPosX(200); print(dist);
+                //終点をつぎの始点にする
                 sp = ep;
             }
         }
+        //distでキャラを動かす
         {
-            t += 0.005f;
-            if (dist==0)t = 0;
-            VECTOR p = bezier(p0, p1, p2, p3, t);
-            VECTOR v = derivative(p0, p1, p2, p3, t);
-            float angle = Atan2(v.y, v.x);
-            arrow(p, p + v*0.2f, red, 5);
-            image(img, p, angle, 1, CENTER);
-        }
-        {
-            dist += 5;
+            float vel = 5;
+            dist += vel;
             if (dist > LUT.back())dist = 0;
             float t = distToT(&LUT.front(), LUT.size(), dist);
             VECTOR p = bezier(p0, p1, p2, p3, t);
             VECTOR v = derivative(p0, p1, p2, p3, t);
             float angle = Atan2(v.y, v.x);
-            arrow(p, p + v * 0.2f, red, 5);
             image(imgBlue, p, angle, 1, CENTER);
-
         }
-        
+        //tでキャラを動かす
+        {
+            float dt = 0.005f;
+            t += dt;
+            if (dist==0)t = 0;//スタートをそろえる
+            VECTOR p = bezier(p0, p1, p2, p3, t);
+            VECTOR v = derivative(p0, p1, p2, p3, t);
+            float angle = Atan2(v.y, v.x);
+            arrow(p, p + v*0.1f, red, 5);
+            image(img, p, angle, 1, CENTER);
+        }
         //Points
         line(p0, p1, white, 1);
-        line(p1, p2, white, 1);
         line(p2, p3, white, 1);
         circle(p0, 14, black, white, 6);
         circle(p1, 14, black, white, 6);
